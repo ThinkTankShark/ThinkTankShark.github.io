613957cb06d58ae4077d2fe0f37e3553c5f19a33
Sepand Assadi <info@sepandassadi.com>
2016-02-23 22:41:22 -0800

    initial design

---
+++ projects/js/grocerylist.js
@@ -0,0 +1,32 @@
+//Create a new list
+function createNewList() {
+ var listName = document.createList.listInput.value;
+
+
+
+
+ alert(list);
+ alert(listName + " Has Been Create");
+}
+
+//Add items to list
+function addItem(item) {
+  alert(item + " Just Added");
+}
+
+//Remove items from list
+function removeItem(item) {
+  alert(item + " Has Been Removed");
+}
+
+//Update the quantity from list
+function updateQuantity(item) {
+  alert(item + " Quantity Has Been Updated");
+}
+
+//print the list
+function printItems() {
+  alert("Items Printed");
+}
+
+
