9f9a3fc2d211bc9311c8345788a208656120254b
Sepand Assadi <info@sepandassadi.com>
2016-02-29 23:45:59 -0800

    optional js

--- js/main.js
+++ js/main.js
@@ -2,3 +2,4 @@
 document.getElementById('blog-wrapper').style.height = "200px !important";
 // document.getElementById('wrapper').style.width = ""
 document.getElementById('blog-wrapper').style.width = "500px !important";
+// document.getElementById('blog-wrapper').style.width = "500px !important";
